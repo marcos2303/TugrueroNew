f4efe77159b76745dc9bdf6d8ad2a67b250b7157
marcos <marcos@marcos-desktop>
2017-06-21 13:31:07 -0430

    API addServicios

--- servicios/clienteapp/addServicio.php
+++ servicios/clienteapp/addServicio.php
@@ -3,10 +3,15 @@ header('Content-Type: application/json; charset:utf-8');
 $values = file_get_contents('php://input');
 $values = json_decode($values, true);
 include('../../autoload_servicios.php');
+/************* Clases a utilizar *******************/
+
 $Servicios = new Servicios();
 $ServiciosClientes = new ServiciosClientes();
 $ServiciosPrecios = new ServiciosPrecios();
 $Baremo = new Baremo();
+/***************************************************/
+/****************Seteo y comprobacion de valores*******************/
+
 $values['IdAplicacion'] = 1;
 $values['IdUsuario'] = 1;
 $values['IdServicioTipo'] = 1;
@@ -15,29 +20,32 @@ $LatitudOrigen = '10.2131868';
 $LongitudOrigen = '-67.8862887';
 $LatitudDestino = '10.212626514872';
 $LongitudDestino = '-67.8864110297';
-
 if(!isset($values['Inicio']) or $values['Inicio']==''){
 	$values['Inicio'] = date('Y-m-d h:i:s');
 }
 
-//calculo de la distancia
 if(!isset($values['KM']) or $values['KM']==''){
+//Calculo de KM en caso que no envie
 $values['KM'] = $Baremo->getDistancia($LatitudOrigen, $LongitudOrigen, $LatitudDestino, $LongitudDestino);
 }
-//se efectua el calculo del baremo automatico
+
+/*********************************************************************/
+
+
+/**********************se efectua el calculo del baremo automatico****************************/
 //calculo el precio sin iva
 $values['PrecioSIvaBaremo'] = $Baremo->calcularOferta( $values['KM'], "Encunetado", '1000', "Atascado en barro o arena.", $values['Inicio']);
 //calculo el precio con iva
 $values['PrecioCIvaBaremo'] = $values['PrecioSIvaBaremo'] * 1.12;
-//insertamos en Servicios
 
+/*************************Creamos el Servicio************************************/
+//Insertamos en Servicios
 if(!$Servicios ->addServicios($values)){
 	$response = array("Error"=>1,"MensajeError" => "Se ha presentado un error. Intente de nuevo.");
 	echo json_encode($response);die;
 }
-$values['IdServicio'] = $Servicios->getIdServicio();
+$values['IdServicio'] = $Servicios->getIdServicio();//Variable IdServicio
 //insertamos en ServiciosCliente
-
 if(!$ServiciosClientes ->addServiciosClientes($values)){
 	$response = array("Error"=>0,"MensajeError" => "Se ha presentado un error. Intente de nuevo.");
 	echo json_encode($response);die;
