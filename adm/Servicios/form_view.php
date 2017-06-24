ec3c9dd199958cc809695c3c53a0567fea1353f5
javier <javier@javier-desktop>
2017-06-24 17:24:12 -0430

    services

--- adm/Servicios/form_view.php
+++ adm/Servicios/form_view.php
@@ -1,6 +1,26 @@
 <?php include('../../view_header_admin.php');?>
-<div class="container-fluid">
-    <h1 class="">Servicios</h1>
-    <a class="btn btn-default"  href="<?php echo full_url."/adm/Servicios/index.php";?>"><i class="fa fa-arrow-circle-left"></i> Regresar</a>
+<div class="">
+   <div class="wrapper">
+      <header class="main-header"></header>
+
+        <aside class="main-sidebar">
+            
+        </aside>
+        <div class="content-wrapper">
+            <a href="#" class="btn btn-default pull-right" data-toggle="push-menu" role="button"><i class="fa fa-map-marker"></i> Mapa</a>
+            
+            
+            
+            
+            
+            
+            
+            <a class="btn btn-default"  href="<?php echo full_url."/adm/Servicios/index.php";?>"><i class="fa fa-arrow-circle-left"></i> Regresar</a>
+
+        </div>
+
+        <div class="control-sidebar-bg"></div>
+    </div>
+
 </div>
 <?php include('../../view_footer_admin.php');?>
