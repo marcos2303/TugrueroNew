2b4d1e3971f2ad220f47013391dfe9e11d260a7e
marcos <marcos@marcos-desktop>
2017-06-29 20:53:02 -0400

    Identacion

--- adm/Listas/lista_gruas.php
+++ adm/Listas/lista_gruas.php
@@ -2,31 +2,31 @@
 <div class="container-fluid">
 	<h3 class="text-center">Grúas</h3>
 	<table id="example" class="table table-striped table-bordered table-responsive" width="100%" cellspacing="0">
-			<thead>
-				<tr>
-          <th>Placa</th>
-          <th>Tipo grúa</th>
-          <th>Marca</th>
-          <th>Modelo</th>
-					<th>Color</th>
-					<th>Año</th>
-					<th>Detalle</th>
-				</tr>
-			</thead>
-			<tfoot>
-				<tr>
-          <th><input id="Placa" name="Placa" type="text"></th>
-					<th><input id="NombreGruaTipo" name="NombreGruaTipo" type="text"></th>
-          <th><input id="NombreMarca" name="NombreMarca" type="text"></th>
-          <th><input id="Modelo" name="Modelo" type="text"></th>
-          <th><input id="Color" name="Color" type="text"></th>
-					<th><input id="Anio" name="Anio" type="text"></th>
-					<th>Detalle</th>
-				</tr>
-			</tfoot>
-		</table>
+		<thead>
+			<tr>
+				<th>Placa</th>
+				<th>Tipo grúa</th>
+				<th>Marca</th>
+				<th>Modelo</th>
+				<th>Color</th>
+				<th>Año</th>
+				<th>Detalle</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th><input id="Placa" name="Placa" type="text"></th>
+				<th><input id="NombreGruaTipo" name="NombreGruaTipo" type="text"></th>
+				<th><input id="NombreMarca" name="NombreMarca" type="text"></th>
+				<th><input id="Modelo" name="Modelo" type="text"></th>
+				<th><input id="Color" name="Color" type="text"></th>
+				<th><input id="Anio" name="Anio" type="text"></th>
+				<th>Detalle</th>
+			</tr>
+		</tfoot>
+	</table>
 </div>
-	<?php //include('../../view_footer_admin.php')?>
+<?php //include('../../view_footer_admin.php')?>
 <script>
 
 
@@ -47,61 +47,61 @@ $(document).ready(function() {
 	} );
 
 
-    var table = $('#example').DataTable({
-          "scrollX": true,
-          "processing": true,
-          "serverSide": true,
-          scrollCollapse: true,
-		      "sDom": 'trp',
-          "ajax": "<?php echo full_url."/adm/Listas/index.php?action=lista_gruas_json&IdProveedor=";if(isset($values['IdProveedor']) and $values['IdProveedor']!='') echo $values['IdProveedor']; ?>",
-		      "language": {
-                "url": "<?php echo full_url."/web/js/"?>datatables.spanish.lang"
-              },
-          "columns": [
-              { "data": "Placa" },
-              { "data": "NombreGruaTipo" },
-              { "data": "NombreMarca" },
-              { "data": "Modelo" },
-              { "data": "Color" },
-              { "data": "Anio" },
-              { "data": "actions" }
-          ],"rowCallback": function( row, data, index ) {
-          },
-        "aoColumnDefs": [
-            { 'bSortable': false, 'aTargets': [ 6 ] }
-         ]
-    });
+	var table = $('#example').DataTable({
+		"scrollX": true,
+		"processing": true,
+		"serverSide": true,
+		scrollCollapse: true,
+		"sDom": 'trp',
+		"ajax": "<?php echo full_url."/adm/Listas/index.php?action=lista_gruas_json&IdProveedor=";if(isset($values['IdProveedor']) and $values['IdProveedor']!='') echo $values['IdProveedor']; ?>",
+		"language": {
+			"url": "<?php echo full_url."/web/js/"?>datatables.spanish.lang"
+		},
+		"columns": [
+			{ "data": "Placa" },
+			{ "data": "NombreGruaTipo" },
+			{ "data": "NombreMarca" },
+			{ "data": "Modelo" },
+			{ "data": "Color" },
+			{ "data": "Anio" },
+			{ "data": "actions" }
+		],"rowCallback": function( row, data, index ) {
+		},
+		"aoColumnDefs": [
+			{ 'bSortable': false, 'aTargets': [ 6 ] }
+		]
+	});
 
-  $('#column_0').on ('keypress', function(e){
-      if(e.which == 13) {
-          table.column(table.column(0)).search($(this).val()).draw();
-      }
-  });
-  $('#column_1').on ('keypress', function(e){
-      if(e.which == 13) {
-          table.column(table.column(1)).search($(this).val()).draw();
-      }
-  });
-  $('#column_2').on ('keypress', function(e){
-      if(e.which == 13) {
-          table.column(table.column(2)).search($(this).val()).draw();
-      }
-  });
-  $('#column_3').on ('keypress', function(e){
-      if(e.which == 13) {
-          table.column(table.column(3)).search($(this).val()).draw();
-      }
-  });
-  $('#column_4').on ('keypress', function(e){
-      if(e.which == 13) {
-          table.column(table.column(4)).search($(this).val()).draw();
-      }
-  });
-  $('#column_5').on ('keypress', function(e){
-      if(e.which == 13) {
-          table.column(table.column(5)).search($(this).val()).draw();
-      }
-  });
+	$('#column_0').on ('keypress', function(e){
+		if(e.which == 13) {
+			table.column(table.column(0)).search($(this).val()).draw();
+		}
+	});
+	$('#column_1').on ('keypress', function(e){
+		if(e.which == 13) {
+			table.column(table.column(1)).search($(this).val()).draw();
+		}
+	});
+	$('#column_2').on ('keypress', function(e){
+		if(e.which == 13) {
+			table.column(table.column(2)).search($(this).val()).draw();
+		}
+	});
+	$('#column_3').on ('keypress', function(e){
+		if(e.which == 13) {
+			table.column(table.column(3)).search($(this).val()).draw();
+		}
+	});
+	$('#column_4').on ('keypress', function(e){
+		if(e.which == 13) {
+			table.column(table.column(4)).search($(this).val()).draw();
+		}
+	});
+	$('#column_5').on ('keypress', function(e){
+		if(e.which == 13) {
+			table.column(table.column(5)).search($(this).val()).draw();
+		}
+	});
 
 	$('#clear').click(function(){
 		table.search( '' ).columns().search( '' ).draw();
--- popups.php
+++ popups.php
@@ -1,117 +1,117 @@
-        <div class="modal modal-default" id="popupSuccess">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                  <img src="">
-              </div>
-              <div class="modal-body">
-                <p>...</p>
-              <div name="progress" class="progress active">
-                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
-                </div>
-              </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-danger"></button>
-                <button type="button" class="btn btn-success"></button>
-              </div>
+<div class="modal modal-default" id="popupSuccess">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <img src="">
+      </div>
+      <div class="modal-body">
+        <p>...</p>
+        <div name="progress" class="progress active">
+          <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-danger"></button>
+        <button type="button" class="btn btn-success"></button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal modal-default" id="popupError">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <img src="">
+      </div>
+      <div class="modal-body">
+        <p>...</p>
+        <div name="progress" class="progress active">
+          <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-danger"></button>
+        <button type="button" class="btn btn-success"></button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal modal-default" id="popupCargando">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <img src="">
+      </div>
+      <div class="modal-body">
+        <p>...</p>
+        <div name="progress" class="progress active">
+          <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-danger"></button>
+        <button type="button" class="btn btn-success"></button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal modal-default" id="popupAutenticacion">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <img src="">
+      </div>
+      <div class="modal-body">
+        <p></p>
+        <h4 class="text-center">Ingrese Usuario y clave para realizar la reasignación</h4>
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="form-group">
+              <label>Usuario:</label>
+              <input class="form-control" type="text" id="Usuario" name="Usuario">
             </div>
+            <div class="form-group">
+              <label>Clave:</label>
+              <input class="form-control" type="password" id="UsuarioClaveEspecial" name="UsuarioClaveEspecial">
+            </div>
+          </div>
+        </div>
+        <div name="progress" class="progress active">
+          <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
           </div>
         </div>
 
-        <div class="modal modal-default" id="popupError">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                  <img src="">
-              </div>
-              <div class="modal-body">
-                <p>...</p>
-              <div name="progress" class="progress active">
-                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
-                </div>
-              </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-danger"></button>
-                <button type="button" class="btn btn-success"></button>
-              </div>
-            </div>
-          </div>
-        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-success"></button>
+        <button type="button" class="btn btn-danger"></button>
+        <button type="button" class="btn btn-success"></button>
+      </div>
+    </div>
+  </div>
+</div>
 
-        <div class="modal modal-default" id="popupCargando">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                  <img src="">
-              </div>
-              <div class="modal-body">
-                <p>...</p>
-              <div name="progress" class="progress active">
-                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
-                </div>
-              </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-danger"></button>
-                <button type="button" class="btn btn-success"></button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="modal modal-default" id="popupAutenticacion">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                  <img src="">
-              </div>
-              <div class="modal-body">
-				<p></p>
-				<h4 class="text-center">Ingrese Usuario y clave para realizar la reasignación</h4>
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="form-group">
-							<label>Usuario:</label>
-							  <input class="form-control" type="text" id="Usuario" name="Usuario">
-						</div>
-						<div class="form-group">
-							<label>Clave:</label>
-							  <input class="form-control" type="password" id="UsuarioClaveEspecial" name="UsuarioClaveEspecial">
-						</div>
-					</div>
-				</div>
-					<div name="progress" class="progress active">
-					  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">                  <span class="sr-only">40% Complete (success)</span>
-					  </div>
-					</div>
-
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-success"></button>
-                <button type="button" class="btn btn-danger"></button>
-                <button type="button" class="btn btn-success"></button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="modal bs-example-modal-lg" tabindex="-1" id="popupListas" role="dialog">
-          <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-              <div class="modal-body">
-				            <p></p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-danger" onclick="closePops();"> Cerrar</button>
-              </div>
-            </div>
-          </div>
-        </div>
+<div class="modal bs-example-modal-lg" tabindex="-1" id="popupListas" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <p></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" onclick="closePops();"> Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
