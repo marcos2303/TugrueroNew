aa1821a46f8847b0c8f3dea607e9af0b3133bb77
javier <javier@javier-desktop>
2017-09-09 16:35:07 -0430

    modificaciones datatables servicios

--- adm/Listas/lista_servicios.php
+++ adm/Listas/lista_servicios.php
@@ -1,67 +1,8 @@
-<table id="example" class="table table-striped table-bordered table-responsive" width="100%" cellspacing="0">
+<table id="example" class="table table-striped table-bordered table-responsive" width="100%">
   <thead class="">
     <tr>
       <th>IdServicio</th>
       <th>Código</th>
-      <th>Aplicacion</th>
-      <th>Tipo servicio</th>
-      <th>Estatus</th>
-      <th>Agendado</th>
-      <th>F.Agendado</th>
-      <th>Usuario</th>
-      <th>NombreAveria</th>
-      <th>AveriaDetalle</th>
-      <th>NombreCondicionLugar</th>
-      <th>CondicionDetalle</th>
-      <th>LatitudOrigen</th>
-      <th>LongitudOrigen</th>
-      <th>NombreEstadoOrigen</th>
-      <th>DireccionOrigen</th>
-      <th>DireccionOrigenDetallada</th>
-      <th>LatitudDestino</th>
-      <th>LongitudDestino</th>
-      <th>NombreEstadoDestino</th>
-      <th>DireccionDestino</th>
-      <th>DireccionDestinoDetallada</th>
-      <th>KM</th>
-      <th>Inicio</th>
-      <th>Fin</th>
-      <th>Observacion</th>
-      <th>UltimaActCliente</th>
-      <th>UltimaActGruero</th>
-      <th>IdentificacionProveedor</th>
-      <th>NombresProveedor</th>
-      <th>ApellidosProveedor</th>
-      <th>NombreProveedorTipo</th>
-      <th>PlacaGrua</th>
-      <th>NombreMarcaGruas</th>
-      <th>ModeloGrua</th>
-      <th>AnioGrua</th>
-      <th>ColorGrua</th>
-      <th>NombresGrua</th>
-      <th>ApellidosGrua</th>
-      <th>CedulaGrua</th>
-      <th>CelularGrua</th>
-      <th>TratoCordial</th>
-      <th>Presencia</th>
-      <th>TratoVehiculo</th>
-      <th>Puntual</th>
-      <th>NombresCliente</th>
-      <th>ApellidosCliente</th>
-      <th>CedulaCliente</th>
-      <th>PlacaCliente</th>
-      <th>ModeloCliente</th>
-      <th>ColorCliente</th>
-      <th>AnioCliente</th>
-      <th>CelularCliente</th>
-      <th>PolizaVencida</th>
-      <th>NombreUsuarioCliente</th>
-      <th>PrecioModificado</th>
-      <th>PrecioSIvaBaremo</th>
-      <th>PrecioCIvaBaremo</th>
-      <th>PrecioSIvaModificado</th>
-      <th>PrecioCIvaModificado</th>
-      <th>NombreUsuarioPrecio</th>
       <th>Detalle</th>
     </tr>
   </thead>
@@ -69,65 +10,6 @@
     <tr>
       <th><input id="IdServicio" name="IdServicio" type="text"></th>
       <th><input id="CodigoServicio" name="CodigoServicio" type="text"></th>
-      <th><input id="NombreAplicacion" name="NombreAplicacion" type="text"></th>
-      <th><input id="NombreServicioTipo" name="NombreServicioTipo" type="text"></th>
-      <th><input id="NombreEstatus" name="NombreEstatus" type="text"></th>
-      <th><input id="Agendado" name="Agendado" type="text"></th>
-      <th><input id="FechaAgendado" name="FechaAgendado" type="text"></th>
-      <th><input id="NombreUsuarioServicio" name="NombreUsuarioServicio" type="text"></th>
-      <th><input id="NombreAveria" name="NombreAveria" type="text"></th>
-      <th><input id="AveriaDetalle" name="AveriaDetalle" type="text"></th>
-      <th><input id="NombreCondicionLugar" name="NombreCondicionLugar" type="text"></th>
-      <th><input id="CondicionDetalle" name="CondicionDetalle" type="text"></th>
-      <th><input id="LatitudOrigen" name="LatitudOrigen" type="text"></th>
-      <th><input id="LongitudOrigen" name="LongitudOrigen" type="text"></th>
-      <th><input id="NombreEstadoOrigen" name="NombreEstadoOrigen" type="text"></th>
-      <th><input id="DireccionOrigen" name="DireccionOrigen" type="text"></th>
-      <th><input id="DireccionOrigenDetallada" name="DireccionOrigenDetallada" type="text"></th>
-      <th><input id="LatitudDestino" name="LatitudDestino" type="text"></th>
-      <th><input id="LongitudDestino" name="LongitudDestino" type="text"></th>
-      <th><input id="NombreEstadoDestino" name="NombreEstadoDestino" type="text"></th>
-      <th><input id="DireccionDestino" name="DireccionDestino" type="text"></th>
-      <th><input id="DireccionDestinoDetallada" name="DireccionDestinoDetallada" type="text"></th>
-      <th><input id="KM" name="KM" type="text"></th>
-      <th><input id="Inicio" name="Inicio" type="text"></th>
-      <th><input id="Fin" name="Fin" type="text"></th>
-      <th><input id="Observacion" name="Observacion" type="text"></th>
-      <th><input id="UltimaActCliente" name="UltimaActCliente" type="text"></th>
-      <th><input id="UltimaActGruero" name="UltimaActGruero" type="text"></th>
-      <th><input id="IdentificacionProveedor" name="IdentificacionProveedor" type="text"></th>
-      <th><input id="NombresProveedor" name="NombresProveedor" type="text"></th>
-      <th><input id="ApellidosProveedor" name="ApellidosProveedor" type="text"></th>
-      <th><input id="NombreProveedorTipo" name="NombreProveedorTipo" type="text"></th>
-      <th><input id="PlacaGrua" name="PlacaGrua" type="text"></th>
-      <th><input id="NombreMarcaGruas" name="NombreMarcaGruas" type="text"></th>
-      <th><input id="ModeloGrua" name="ModeloGrua" type="text"></th>
-      <th><input id="AnioGrua" name="AnioGrua" type="text"></th>
-      <th><input id="ColorGrua" name="ColorGrua" type="text"></th>
-      <th><input id="NombresGrua" name="NombresGrua" type="text"></th>
-      <th><input id="ApellidosGrua" name="ApellidosGrua" type="text"></th>
-      <th><input id="CedulaGrua" name="CedulaGrua" type="text"></th>
-      <th><input id="CelularGrua" name="CelularGrua" type="text"></th>
-      <th><input id="TratoCordial" name="TratoCordial" type="text"></th>
-      <th><input id="Presencia" name="Presencia" type="text"></th>
-      <th><input id="TratoVehiculo" name="TratoVehiculo" type="text"></th>
-      <th><input id="Puntual" name="Puntual" type="text"></th>
-      <th><input id="NombresCliente" name="NombresCliente" type="text"></th>
-      <th><input id="ApellidosCliente" name="ApellidosCliente" type="text"></th>
-      <th><input id="CedulaCliente" name="CedulaCliente" type="text"></th>
-      <th><input id="PlacaCliente" name="PlacaCliente" type="text"></th>
-      <th><input id="ModeloCliente" name="ModeloCliente" type="text"></th>
-      <th><input id="ColorCliente" name="ColorCliente" type="text"></th>
-      <th><input id="AnioCliente" name="AnioCliente" type="text"></th>
-      <th><input id="CelularCliente" name="CelularCliente" type="text"></th>
-      <th><input id="PolizaVencida" name="PolizaVencida" type="text"></th>
-      <th><input id="NombreUsuarioCliente" name="NombreUsuarioCliente" type="text"></th>
-      <th><input id="PrecioModificado" name="PrecioModificado" type="text"></th>
-      <th><input id="PrecioSIvaBaremo" name="PrecioSIvaBaremo" type="text"></th>
-      <th><input id="PrecioCIvaBaremo" name="PrecioCIvaBaremo" type="text"></th>
-      <th><input id="PrecioSIvaModificado" name="PrecioSIvaModificado" type="text"></th>
-      <th><input id="PrecioCIvaModificado" name="PrecioCIvaModificado" type="text"></th>
-      <th><input id="NombreUsuarioPrecio" name="NombreUsuarioPrecio" type="text"></th>
       <th>Detalle</th>
     </tr>
   </tfoot>
@@ -158,6 +40,7 @@ $(document).ready(function() {
     "processing": true,
     "sServerMethod": "POST",
     "serverSide": true,
+    "bAutoWidth": true,
     "sScrollY": "260",
     "sDom": 'Btrp',
     "ajax":link_servidor + "/adm/Listas/index.php?action=lista_servicios_json&IdProveedor=<?php echo $values['IdProveedor'];?>&IdGrua=<?php echo $values['IdGrua'];?>&EditarServicio=<?php echo $values['EditarServicio'];?>",
@@ -165,153 +48,23 @@ $(document).ready(function() {
       "url": link_servidor + "/web/js/datatables.spanish.lang"
     },
 
-    buttons: [
-            {
-                extend: 'colvis',
-                collectionLayout: 'fixed two-column'
-            },
-      {
-        extend: 'colvisGroup',
-        text: 'Básicos',
-        show: [ 1,2,3,4,5,22,23,60 ],
-        hide: [ 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-          21,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-          41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
-        },
-        {
-          extend: 'colvisGroup',
-          text: 'Cliente',
-          show: [ 45,46,47,48,49,50,51 ],
-          hide: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-            21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-            41,42,43,44,52,53,54,55,56,57,58,59,60]
-          },
-          {
-            extend: 'colvisGroup',
-            text: 'Gruero',
-            show: [ 31,32,33,35,36,37,38,39,61 ],
-            hide: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-              21,22,23,24,25,26,27,28,29,30,34,40,
-              41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
-            },
-            {
-              extend: 'colvisGroup',
-              text: 'Averia/Condición',
-              show: [ 7,8,9,10,61 ],
-              hide: [ 1,2,3,4,5,6,11,12,13,14,15,16,17,18,19,20,
-                21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-                41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
-              },
-              {
-                extend: 'colvisGroup',
-                text: 'Ubicación/Distancia',
-                show: [ 13,14,15,18,19,20,21 ],
-                hide: [ 1,2,3,4,5,6,7,8,9,10,11,12,16,17,22,23,24,25,26,
-                  27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-                  41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
-                },
-                {
-                  extend: 'colvisGroup',
-                  text: 'Precios',
-                  show: [ 54,55,56,57,58,59,61 ],
-                  hide: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-                    21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-                    41,42,43,44,45,46,47,48,49,50,51,52,53,60]
-                  },
-
-                  {
-                    extend: 'colvisGroup',
-                    text: 'Todos',
-                    show: ':hidden'
-                  }
-                ],
+buttons: [
+            'colvis'
+        ],
                 "columns": [
                   { "data" : "IdServicio" },
                   { "data" : "CodigoServicio" },
-                  { "data" : "NombreAplicacion" },
-                  { "data" : "NombreServicioTipo" },
-                  { "data" : "NombreEstatus" },
-                  { "data" : "Agendado" },
-                  { "data" : "FechaAgendado" },
-                  { "data" : "NombreUsuarioServicio" },
-                  { "data" : "NombreAveria" },
-                  { "data" : "AveriaDetalle" },
-                  { "data" : "NombreCondicionLugar" },
-                  { "data" : "CondicionDetalle" },
-                  { "data" : "LatitudOrigen" },
-                  { "data" : "LongitudOrigen" },
-                  { "data" : "NombreEstadoOrigen" },
-                  { "data" : "DireccionOrigen" },
-                  { "data" : "DireccionOrigenDetallada" },
-                  { "data" : "LatitudDestino" },
-                  { "data" : "LongitudDestino" },
-                  { "data" : "NombreEstadoDestino" },
-                  { "data" : "DireccionDestino" },
-                  { "data" : "DireccionDestinoDetallada" },
-                  { "data" : "KM" },
-                  { "data" : "Inicio" },
-                  { "data" : "Fin" },
-                  { "data" : "Observacion" },
-                  { "data" : "UltimaActCliente" },
-                  { "data" : "UltimaActGruero" },
-                  /************Datos ServiciosGruas, Proveedores y Gruas*******************/
-                  { "data" : "IdentificacionProveedor" },
-                  { "data" : "NombresProveedor" },
-                  { "data" : "ApellidosProveedor" },
-                  { "data" : "NombreProveedorTipo" },
-                  { "data" : "PlacaGrua" },
-                  { "data" : "NombreMarcaGruas" },
-                  { "data" : "ModeloGrua" },
-                  { "data" : "AnioGrua" },
-                  { "data" : "ColorGrua" },
-                  { "data" : "NombresGrua" },
-                  { "data" : "ApellidosGrua" },
-                  { "data" : "CedulaGrua" },
-                  { "data" : "CelularGrua" },
-                  { "data" : "TratoCordial" },
-                  { "data" : "Presencia" },
-                  { "data" : "TratoVehiculo" },
-                  { "data" : "Puntual" },
-                  /**********Datos ServiciosClientes****************************/
-                  { "data" : "NombresCliente" },
-                  { "data" : "ApellidosCliente" },
-                  { "data" : "CedulaCliente" },
-                  { "data" : "PlacaCliente" },
-                  { "data" : "ModeloCliente" },
-                  { "data" : "ColorCliente" },
-                  { "data" : "AnioCliente" },
-                  { "data" : "CelularCliente" },
-                  { "data" : "PolizaVencida" },
-                  { "data" : "NombreUsuarioCliente" },
-                  /*****************ServiciosPrecios*************************************/
-                  { "data" : "PrecioModificado" },
-                  { "data" : "PrecioSIvaBaremo" },
-                  { "data" : "PrecioCIvaBaremo" },
-                  { "data" : "PrecioSIvaModificado" },
-                  { "data" : "PrecioCIvaModificado" },
-                  { "data" : "NombreUsuarioPrecio" },
                   { "data" : "actions" },
                 ],
                 "aoColumnDefs": [
-                  { "visible": false, "targets": [ 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-                    21,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-                    41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
+                  { "visible": false, "targets": []
                   },
-                  { 'bSortable': false, 'aTargets': [ 61 ] }
+                  { 'bSortable': false, 'aTargets': [ 2 ] }
                 ]
               });
-                $('#example').css( 'display', 'table' );
+                 new FixedColumns( table );
+                //$('#example').css( 'display', 'table' );
 
-              /*for(i=0; i< table.columns()[0].length;i++){
-
-              $('#column_'+ i).on('keypress', function(e){
-
-              if(e.which == 13) {
-              table.column(table.column(i)).search($(this).val()).draw();
-              console.log('#column_'+ i);
-            }
-          });
-        }*/
 	//click
     $('#example tbody').on( 'click', 'tr', function () {
         var data = table.row( this ).data();
@@ -327,7 +80,6 @@ $(document).ready(function() {
 	$('#example tbody').on('dblclick', 'tr', function () {
 	var data = table.row( this ).data();
 	$(this).addClass('seleccionado');
-	console.log(data.IdServicio);
 
 	$(location).attr('href', '<?php echo full_url."/adm/Servicios/index.php?action=edit&IdServicio="?>' + data.IdServicio);
 	} );
